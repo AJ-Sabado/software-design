6eb99b7fba671d531e11595faa621d3f4a80a4bd
Arvin Joseph Sabado <a.sabado.522151@umindanao.edu.ph>
2025-02-06 09:21:45 +0800

    2nd commit

--- lab2/Form1.Designer.cs
+++ lab2/Form1.Designer.cs
@@ -34,19 +34,26 @@
 			textBoxArrayInput = new TextBox();
 			buttonCalculateSum = new Button();
 			labelSumResult = new Label();
+			buttonFibonacci = new Button();
+			labelFibonacci = new Label();
+			textBoxFibonacci = new TextBox();
+			textBoxPower = new TextBox();
+			labelPower = new Label();
+			buttonCalculatePower = new Button();
 			SuspendLayout();
 			// 
 			// textBoxInputN
 			// 
-			textBoxInputN.Location = new Point(217, 168);
+			textBoxInputN.Location = new Point(458, 90);
 			textBoxInputN.Name = "textBoxInputN";
 			textBoxInputN.Size = new Size(100, 23);
 			textBoxInputN.TabIndex = 0;
+			textBoxInputN.Text = "1";
 			// 
 			// labelResult
 			// 
 			labelResult.AutoSize = true;
-			labelResult.Location = new Point(217, 218);
+			labelResult.Location = new Point(458, 140);
 			labelResult.Name = "labelResult";
 			labelResult.Size = new Size(55, 15);
 			labelResult.TabIndex = 1;
@@ -54,7 +61,7 @@
 			// 
 			// buttonCalculateFactorial
 			// 
-			buttonCalculateFactorial.Location = new Point(217, 260);
+			buttonCalculateFactorial.Location = new Point(458, 182);
 			buttonCalculateFactorial.Name = "buttonCalculateFactorial";
 			buttonCalculateFactorial.Size = new Size(100, 23);
 			buttonCalculateFactorial.TabIndex = 2;
@@ -64,14 +71,15 @@
 			// 
 			// textBoxArrayInput
 			// 
-			textBoxArrayInput.Location = new Point(483, 168);
+			textBoxArrayInput.Location = new Point(243, 245);
 			textBoxArrayInput.Name = "textBoxArrayInput";
 			textBoxArrayInput.Size = new Size(100, 23);
 			textBoxArrayInput.TabIndex = 3;
+			textBoxArrayInput.Text = "1,2,3";
 			// 
 			// buttonCalculateSum
 			// 
-			buttonCalculateSum.Location = new Point(483, 260);
+			buttonCalculateSum.Location = new Point(244, 337);
 			buttonCalculateSum.Name = "buttonCalculateSum";
 			buttonCalculateSum.Size = new Size(100, 23);
 			buttonCalculateSum.TabIndex = 4;
@@ -82,17 +90,77 @@
 			// labelSumResult
 			// 
 			labelSumResult.AutoSize = true;
-			labelSumResult.Location = new Point(483, 218);
+			labelSumResult.Location = new Point(244, 295);
 			labelSumResult.Name = "labelSumResult";
 			labelSumResult.Size = new Size(34, 15);
 			labelSumResult.TabIndex = 5;
 			labelSumResult.Text = "Sum:";
+			// 
+			// buttonFibonacci
+			// 
+			buttonFibonacci.Location = new Point(243, 182);
+			buttonFibonacci.Name = "buttonFibonacci";
+			buttonFibonacci.Size = new Size(100, 23);
+			buttonFibonacci.TabIndex = 6;
+			buttonFibonacci.Text = "Solve Fibonacci";
+			buttonFibonacci.UseVisualStyleBackColor = true;
+			buttonFibonacci.Click += buttonFibonacci_Click;
+			// 
+			// labelFibonacci
+			// 
+			labelFibonacci.AutoSize = true;
+			labelFibonacci.Location = new Point(243, 140);
+			labelFibonacci.Name = "labelFibonacci";
+			labelFibonacci.Size = new Size(61, 15);
+			labelFibonacci.TabIndex = 7;
+			labelFibonacci.Text = "Fibonacci:";
+			// 
+			// textBoxFibonacci
+			// 
+			textBoxFibonacci.Location = new Point(243, 90);
+			textBoxFibonacci.Name = "textBoxFibonacci";
+			textBoxFibonacci.Size = new Size(100, 23);
+			textBoxFibonacci.TabIndex = 8;
+			textBoxFibonacci.Text = "1";
+			// 
+			// textBoxPower
+			// 
+			textBoxPower.Location = new Point(458, 245);
+			textBoxPower.Name = "textBoxPower";
+			textBoxPower.Size = new Size(100, 23);
+			textBoxPower.TabIndex = 9;
+			textBoxPower.Text = "2^2";
+			// 
+			// labelPower
+			// 
+			labelPower.AutoSize = true;
+			labelPower.Location = new Point(458, 295);
+			labelPower.Name = "labelPower";
+			labelPower.Size = new Size(145, 15);
+			labelPower.TabIndex = 10;
+			labelPower.Text = "X raised to the power of n:";
+			// 
+			// buttonCalculatePower
+			// 
+			buttonCalculatePower.Location = new Point(458, 337);
+			buttonCalculatePower.Name = "buttonCalculatePower";
+			buttonCalculatePower.Size = new Size(100, 23);
+			buttonCalculatePower.TabIndex = 11;
+			buttonCalculatePower.Text = "Solve Power";
+			buttonCalculatePower.UseVisualStyleBackColor = true;
+			buttonCalculatePower.Click += buttonCalculatePower_Click;
 			// 
 			// Form1
 			// 
 			AutoScaleDimensions = new SizeF(7F, 15F);
 			AutoScaleMode = AutoScaleMode.Font;
 			ClientSize = new Size(800, 450);
+			Controls.Add(buttonCalculatePower);
+			Controls.Add(labelPower);
+			Controls.Add(textBoxPower);
+			Controls.Add(textBoxFibonacci);
+			Controls.Add(labelFibonacci);
+			Controls.Add(buttonFibonacci);
 			Controls.Add(labelSumResult);
 			Controls.Add(buttonCalculateSum);
 			Controls.Add(textBoxArrayInput);
@@ -114,5 +182,11 @@
 		private TextBox textBoxArrayInput;
 		private Button buttonCalculateSum;
 		private Label labelSumResult;
+		private Button buttonFibonacci;
+		private Label labelFibonacci;
+		private TextBox textBoxFibonacci;
+		private TextBox textBoxPower;
+		private Label labelPower;
+		private Button buttonCalculatePower;
 	}
 }
--- lab2/Form1.cs
+++ lab2/Form1.cs
@@ -18,6 +18,21 @@ namespace lab2
 			if (n == 0) return numbers[0];
 			return numbers[n] + RecursiveSum(numbers, n - 1);
 		}
+
+		public int RecursiveFibonacci(int n)
+		{
+			if (n < 1) return 0;
+			if (n == 1) return 1;
+			return RecursiveFibonacci(n - 1) + RecursiveFibonacci(n - 2);
+		}
+
+		public int RecursivePower(int x, int n)
+		{
+			if (n == 1) return x;
+			return x * RecursivePower(x, n - 1);
+		}
+
+
 		private void Form1_Load(object sender, EventArgs e)
 		{
 
@@ -35,6 +50,20 @@ namespace lab2
 			int[] numbers = textBoxArrayInput.Text.Split(',').Select(int.Parse).ToArray();
 			int result = RecursiveSum(numbers, numbers.Length - 1);
 			labelSumResult.Text = $"Sum: {result}";
+		}
+
+		private void buttonFibonacci_Click(object sender, EventArgs e)
+		{
+			int n = int.Parse(textBoxFibonacci.Text);
+			int result = RecursiveFibonacci(n);
+			labelFibonacci.Text = $"Fibonacci: {result}";
+		}
+
+		private void buttonCalculatePower_Click(object sender, EventArgs e)
+		{
+			int[] input = textBoxPower.Text.Split('^').Select(int.Parse).ToArray();
+			int result = RecursivePower(input[0], input[1]);
+			labelPower.Text = $"{input[0]} raised to the power of {input[1]}: {result}"; 
 		}
 	}
 }
