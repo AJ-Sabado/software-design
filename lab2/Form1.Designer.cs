031cc898a5f2aa5cbc2baa06b43f3e976d2bcb0d
Arvin Joseph Sabado <a.sabado.522151@umindanao.edu.ph>
2025-02-06 08:49:24 +0800

    Lab 2 1st commit

--- lab2/Form1.Designer.cs
+++ lab2/Form1.Designer.cs
@@ -28,12 +28,91 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
+			textBoxInputN = new TextBox();
+			labelResult = new Label();
+			buttonCalculateFactorial = new Button();
+			textBoxArrayInput = new TextBox();
+			buttonCalculateSum = new Button();
+			labelSumResult = new Label();
+			SuspendLayout();
+			// 
+			// textBoxInputN
+			// 
+			textBoxInputN.Location = new Point(217, 168);
+			textBoxInputN.Name = "textBoxInputN";
+			textBoxInputN.Size = new Size(100, 23);
+			textBoxInputN.TabIndex = 0;
+			// 
+			// labelResult
+			// 
+			labelResult.AutoSize = true;
+			labelResult.Location = new Point(217, 218);
+			labelResult.Name = "labelResult";
+			labelResult.Size = new Size(55, 15);
+			labelResult.TabIndex = 1;
+			labelResult.Text = "Factorial:";
+			// 
+			// buttonCalculateFactorial
+			// 
+			buttonCalculateFactorial.Location = new Point(217, 260);
+			buttonCalculateFactorial.Name = "buttonCalculateFactorial";
+			buttonCalculateFactorial.Size = new Size(100, 23);
+			buttonCalculateFactorial.TabIndex = 2;
+			buttonCalculateFactorial.Text = "Solve Factorial";
+			buttonCalculateFactorial.UseVisualStyleBackColor = true;
+			buttonCalculateFactorial.Click += buttonCalculateFactorial_Click;
+			// 
+			// textBoxArrayInput
+			// 
+			textBoxArrayInput.Location = new Point(483, 168);
+			textBoxArrayInput.Name = "textBoxArrayInput";
+			textBoxArrayInput.Size = new Size(100, 23);
+			textBoxArrayInput.TabIndex = 3;
+			// 
+			// buttonCalculateSum
+			// 
+			buttonCalculateSum.Location = new Point(483, 260);
+			buttonCalculateSum.Name = "buttonCalculateSum";
+			buttonCalculateSum.Size = new Size(100, 23);
+			buttonCalculateSum.TabIndex = 4;
+			buttonCalculateSum.Text = "Solve Sum";
+			buttonCalculateSum.UseVisualStyleBackColor = true;
+			buttonCalculateSum.Click += buttonCalculateSum_Click;
+			// 
+			// labelSumResult
+			// 
+			labelSumResult.AutoSize = true;
+			labelSumResult.Location = new Point(483, 218);
+			labelSumResult.Name = "labelSumResult";
+			labelSumResult.Size = new Size(34, 15);
+			labelSumResult.TabIndex = 5;
+			labelSumResult.Text = "Sum:";
+			// 
+			// Form1
+			// 
+			AutoScaleDimensions = new SizeF(7F, 15F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(800, 450);
+			Controls.Add(labelSumResult);
+			Controls.Add(buttonCalculateSum);
+			Controls.Add(textBoxArrayInput);
+			Controls.Add(buttonCalculateFactorial);
+			Controls.Add(labelResult);
+			Controls.Add(textBoxInputN);
+			Name = "Form1";
+			Text = "Form1";
+			Load += Form1_Load;
+			ResumeLayout(false);
+			PerformLayout();
 		}
 
 		#endregion
+
+		private TextBox textBoxInputN;
+        private Label labelResult;
+        private Button buttonCalculateFactorial;
+		private TextBox textBoxArrayInput;
+		private Button buttonCalculateSum;
+		private Label labelSumResult;
 	}
 }
--- lab2/Form1.cs
+++ lab2/Form1.cs
@@ -6,5 +6,35 @@ namespace lab2
 		{
 			InitializeComponent();
 		}
+
+		public int RecursiveFactorial(int n)
+		{
+			if (n == 0) return 1;
+			return n * RecursiveFactorial(n - 1);
+		}
+
+		public int RecursiveSum(int[] numbers, int n)
+		{
+			if (n == 0) return numbers[0];
+			return numbers[n] + RecursiveSum(numbers, n - 1);
+		}
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void buttonCalculateFactorial_Click(object sender, EventArgs e)
+		{
+			int number = int.Parse(textBoxInputN.Text);
+			int result = RecursiveFactorial(number);
+			labelResult.Text = $"Factorial: {result}";
+		}
+
+		private void buttonCalculateSum_Click(object sender, EventArgs e)
+		{
+			int[] numbers = textBoxArrayInput.Text.Split(',').Select(int.Parse).ToArray();
+			int result = RecursiveSum(numbers, numbers.Length - 1);
+			labelSumResult.Text = $"Sum: {result}";
+		}
 	}
 }
