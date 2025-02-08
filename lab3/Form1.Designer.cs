9bc1c5316071583b320090488ae8d3dbd72aa0f5
AJ-Sabado <a.sabado.522151@umindanao.edu.ph>
2025-02-08 15:07:38 +0800

    Laboratory 3

--- lab3/Form1.Designer.cs
+++ lab3/Form1.Designer.cs
@@ -20,20 +20,65 @@
 			base.Dispose(disposing);
 		}
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		/// <summary>
-		///  Required method for Designer support - do not modify
-		///  the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
-		}
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            buttonShowInfo = new Button();
+            buttonShowBookList = new Button();
+            listBoxResults = new ListBox();
+            SuspendLayout();
+            // 
+            // buttonShowInfo
+            // 
+            buttonShowInfo.Location = new Point(35, 37);
+            buttonShowInfo.Name = "buttonShowInfo";
+            buttonShowInfo.Size = new Size(90, 40);
+            buttonShowInfo.TabIndex = 0;
+            buttonShowInfo.Text = "Show Info";
+            buttonShowInfo.UseVisualStyleBackColor = true;
+            buttonShowInfo.Click += buttonShowInfo_Click;
+            // 
+            // buttonShowBookList
+            // 
+            buttonShowBookList.Location = new Point(35, 114);
+            buttonShowBookList.Name = "buttonShowBookList";
+            buttonShowBookList.Size = new Size(90, 40);
+            buttonShowBookList.TabIndex = 1;
+            buttonShowBookList.Text = "Display Books";
+            buttonShowBookList.UseVisualStyleBackColor = true;
+            buttonShowBookList.Click += buttonShowBookList_Click;
+            // 
+            // listBoxResults
+            // 
+            listBoxResults.FormattingEnabled = true;
+            listBoxResults.ItemHeight = 15;
+            listBoxResults.Location = new Point(131, 37);
+            listBoxResults.Name = "listBoxResults";
+            listBoxResults.Size = new Size(657, 409);
+            listBoxResults.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(listBoxResults);
+            Controls.Add(buttonShowBookList);
+            Controls.Add(buttonShowInfo);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
 
-		#endregion
-	}
+        #endregion
+
+        private Button buttonShowInfo;
+        private Button buttonShowBookList;
+        private ListBox listBoxResults;
+    }
 }
