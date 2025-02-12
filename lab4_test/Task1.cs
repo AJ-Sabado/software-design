2db17c3d663f9bde66e1df6235af0eaa2d72fca2
AJ-Sabado <a.sabado.522151@umindanao.edu.ph>
2025-02-12 19:27:02 +0800

    Created test project

---
+++ lab4_test/Task1.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Models;
+
+namespace lab4_test
+{
+    [TestClass]
+    public sealed class Task1
+    {
+        [TestMethod]
+        public void TestBookCreation()
+        {
+            var book = new Book()
+            {
+                Title = "C# Programming",
+                Author = "John Smith"
+            };
+            Assert.AreEqual("C# Programming", book.Title);
+            Assert.AreEqual("John Smith", book.Author);
+        }
+        [TestMethod]
+        public void TestMagaizineCreation()
+        {
+            var magazine = new Magazine()
+            {
+                Title = "Tech Weekly",
+                Author = "Jane Doe",
+                IssueNumber = 12
+            };
+            Assert.AreEqual("Tech Weekly", magazine.Title);
+            Assert.AreEqual(12, magazine.IssueNumber);
+
+        }
+    }
+}
