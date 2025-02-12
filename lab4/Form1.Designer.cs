e092914ed2c2323d00e8efdc1eb3518223ce079c
AJ-Sabado <a.sabado.522151@umindanao.edu.ph>
2025-02-12 18:02:35 +0800

    Copied week 3 ui

--- lab4/Form1.Designer.cs
+++ lab4/Form1.Designer.cs
@@ -28,19 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
+            listBoxResults = new ListBox();
+            buttonShowBookList = new Button();
+            buttonShowInfo = new Button();
             SuspendLayout();
+            // 
+            // listBoxResults
+            // 
+            listBoxResults.FormattingEnabled = true;
+            listBoxResults.ItemHeight = 15;
+            listBoxResults.Location = new Point(120, 21);
+            listBoxResults.Name = "listBoxResults";
+            listBoxResults.Size = new Size(657, 409);
+            listBoxResults.TabIndex = 5;
+            // 
+            // buttonShowBookList
+            // 
+            buttonShowBookList.Location = new Point(24, 98);
+            buttonShowBookList.Name = "buttonShowBookList";
+            buttonShowBookList.Size = new Size(90, 40);
+            buttonShowBookList.TabIndex = 4;
+            buttonShowBookList.Text = "Display Books";
+            buttonShowBookList.UseVisualStyleBackColor = true;
+            // 
+            // buttonShowInfo
+            // 
+            buttonShowInfo.Location = new Point(24, 21);
+            buttonShowInfo.Name = "buttonShowInfo";
+            buttonShowInfo.Size = new Size(90, 40);
+            buttonShowInfo.TabIndex = 3;
+            buttonShowInfo.Text = "Show Info";
+            buttonShowInfo.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(listBoxResults);
+            Controls.Add(buttonShowBookList);
+            Controls.Add(buttonShowInfo);
             Name = "Form1";
-            Text = "Form1";
+            Text = "Lab 4";
             Load += Form1_Load;
             ResumeLayout(false);
         }
 
         #endregion
+
+        private ListBox listBoxResults;
+        private Button buttonShowBookList;
+        private Button buttonShowInfo;
     }
 }
